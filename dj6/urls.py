cb185d56518074bd63a31a63cd59ae4094d5b202
HQM <nur.hakim.arif@gmail.com>
2026-01-03 18:05:26 +0700

    variable

--- dj6/urls.py
+++ dj6/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from old.views import home
+from old.views import home, variable
 
 urlpatterns = [
     path('', home, name='home'),
+    path('variable', variable, name='variable'),
 ]
--- old/views.py
+++ old/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def variable(request):
+    context = {'username': 'Hakim'}
+    return render(request, 'variable.html', context)
