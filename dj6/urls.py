de8f0cdbd416bfe205b1400a820d5f7ac1db0112
HQM <nur.hakim.arif@gmail.com>
2026-01-03 19:04:44 +0700

    extends tag

--- dj6/urls.py
+++ dj6/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from old.views import home, variable
+from old.views import home, tag, variable
 
 urlpatterns = [
     path('', home, name='home'),
     path('variable', variable, name='variable'),
+    path('tags', tag, name='tags'), 
 ]
--- old/views.py
+++ old/views.py
@@ -4,5 +4,20 @@ def home(request):
     return render(request, 'home.html')
 
 def variable(request):
-    context = {'username': 'Hakim'}
+    context = {'username': 'Hakim', 
+               'user':{
+                    'first_name': 'Hakim',
+                    'age': 30,
+                }}
+    
     return render(request, 'variable.html', context)
+
+def tag(request):
+    context = {
+        'items': ['Item 1', 'Item 2', 'Item 3'],
+        'user': {
+                'is_authenticated': True,
+                'first_name': 'Hakim'
+            }
+        }
+    return render(request, 'tags.html', context=context)
